18f3a6ff8f5d6962915c90f354999f493f5bef0b
Andrew <andrewmosk@icloud.com>
2024-04-07 00:02:55 +0300

    Тесты автодополнения

---
+++ tests/integration_tests/test_completions.py
@@ -0,0 +1,25 @@
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.parametrize(
+    "text, expected", [
+        pytest.param("", [], id="empty"),
+        pytest.param("Я хочу петь", ["и танцевать!"], id="smoke"),
+        pytest.param(
+            "Я хочу петь", ["и танцевать!", "с тобой"],
+            id="smoke multi answer"
+        ),
+        pytest.param("Я хочу\nпеть песни", ["и танцевать!"], id="multiline"),
+    ]
+)
+@pytest.mark.asyncio
+async def test_create_completion(authorized_client: AsyncClient, mocker, text: str, expected: list[str]):
+    mocker.patch("app.api.routers.completions.get_llm_lyrics_completions", return_value=expected)
+    response = await authorized_client.post(
+        "/completions/",
+        json={"text": text},
+    )
+    assert response.status_code == 200
+    expected_response = [{"completion": completion} for completion in expected]
+    assert response.json() == expected_response
