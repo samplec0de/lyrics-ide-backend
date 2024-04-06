4a7721534a26967a3100cca13ac01276bc49139c
Andrew <andrewmosk@icloud.com>
2024-04-07 00:14:51 +0300

    [1.24.0] Удален endpoint TipTap, который выдавал токен на все тексты, ускорен вход через Яндекс

--- app/api/routers/tiptap.py
+++ app/api/routers/tiptap.py
@@ -12,23 +12,6 @@ from app.status_codes import TEXT_NO_PERMISSIONS
 router = APIRouter()
 
 
-@router.get("/token", response_model=Token, operation_id="get_tiptap_access_token", deprecated=True)
-async def get_tiptap_access_token():
-    """Получение JWT токена для TipTap.
-    {"iat": <utcnow()>, "iss": "https://lyrics-backend.k8s-1.sslane.ru", "nbf": <utcnow()>, "aud": <tiptap-app-id>}
-    """
-    access_token = create_access_token(
-        data={
-            "iat": datetime.utcnow(),
-            "iss": "https://lyrics-backend.k8s-1.sslane.ru",
-            "nbf": datetime.utcnow(),
-            "aud": settings.tiptap_app_id,
-        },
-        secret_key=settings.tiptap_secret_key,
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
 @router.get(
     "/token/{text_id}",
     responses={
